3a0ab79929c85480eba907e2821d073c2d64c7d5
Alex <alexkachan@protonmail.com>
2024-02-18 23:47:24 -0600

    first commit

---
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+    //making an array of questions from string file. I think this could be an area of significant
+    //improvement
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //getting items from view
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+        trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
+        }
+        falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
+        }
+        nextButton.setOnClickListener {
+            //update index of array then run get next question
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+
+    }
+    private fun updateQuestion() {
+        //sets the text of questionview as the next question in the array of questions
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        //gets the correct answer for current question, checks user input, then makes a toast
+        //with whether the user got it right or wrong
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+}
